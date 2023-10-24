7ad74a610d4768710907274d4f70883bafdbec3b
Alexander Soldatov <lxndr.soldatov@gmail.com>
2023-10-24 19:07:52 +0300

    refactor: move util function to ui_kit
    ui_kit is correct place for ui/graphical stuff

--- packages/ui_kit/lib/src/utils/utility_functions.dart
+++ packages/ui_kit/lib/src/utils/utility_functions.dart
@@ -1,8 +1,16 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void setStatusBarColorSameAsColorSchemeBackground(BuildContext ctx) {
   final theme = Theme.of(ctx);
+  log('theme.brightness: ${theme.brightness} ',
+      name:
+          'lCargo:ui_kit:utility_functions:setStatusBarColorSameAsColorSchemeBackground');
+  log('theme.colorScheme.background: ${theme.colorScheme.background} ',
+      name:
+          'lCargo:ui_kit:utility_functions:setStatusBarColorSameAsColorSchemeBackground');
   theme.brightness == Brightness.light
       ? SystemChrome.setSystemUIOverlayStyle(
           SystemUiOverlayStyle.dark.copyWith(
